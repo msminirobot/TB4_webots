56313e95a3ec3ebb3de41e682a40f2aab922f2fc
Tanja Kaiser <tanjakkaiser@gmail.com>
2024-08-16 15:35:30 +0200

    initial setup for TB4 including camera and LiDAR settings

---
+++ controllers/test/test.py
@@ -0,0 +1,46 @@
+from controller import Robot, DistanceSensor, Motor, Lidar, Camera 
+TIME_STEP=32
+
+# robot instance.
+robot = Robot()
+
+# motors 
+leftMotor = robot.getDevice('left_wheel_joint')
+rightMotor = robot.getDevice('right_wheel_joint')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
+
+
+# lidar 
+lidar = robot.getDevice('rplidar') 
+lidar.enable(TIME_STEP)
+
+# bumper 
+#bumper = robot.getDevice('bumper_center') 
+#bumper.enable(TIME_STEP)
+
+# camera 
+camera = robot.getDevice('rgb_camera')
+camera.enable(TIME_STEP)
+
+# feedback loop: step simulation until receiving an exit event
+while robot.step(TIME_STEP) != -1:
+
+    leftMotor.setVelocity(1.0)
+    rightMotor.setVelocity(1.0)
+    
+    #get LiDAR ranges 
+    ranges = lidar.getRangeImage()
+    
+    #get camera image 
+    #image is coded as a sequence of four bytes representing the blue, green, red and alpha levels of a pixel.
+    #img = camera.getImage() 
+    
+   # bp = bumper.getValue()
+   # print(bp)
+    
+    # read sensors outputs
+    # process behavior
+    # write actuators inputs
